addf7bb0264d32b218cea64e2289f4d345bc9ee0
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-15 06:06:56 +0000

    [add]管理者ユーザ

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,25 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users, controllers: {
     sessions: "public/sessions",
     passwords: "public/passwords",
     registrations: "public/registrations"
   }
-  
+
   namespace :admin do
     root "homes#top"
+    get "/search" => "searches#search", as: "search"
+    resources :users, only:[:index, :show, :edit, :update] do
+      get "/relationship/followings" => "relationships#followings", as: "followings"
+      get "/relationship/followers" => "relationships#followers", as: "followers"
+    end
+    resources :lists, only:[:show, :edit, :destroy] do
+      resources :list_comments, only:[:destroy]
+    end
   end
-  
+
   scope module: :public do
     root "homes#top"
     get "/about" => "homes#about"
@@ -32,5 +40,5 @@ Rails.application.routes.draw do
       resource :favorites, only:[:create, :destroy]
     end
   end
-  
+
 end
