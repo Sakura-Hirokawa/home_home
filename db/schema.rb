c3e0e37485f45c42008ed7b56f4ab5e696d96c00
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-15 06:40:41 +0000

    [add]フォロー機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_062955) do
+ActiveRecord::Schema.define(version: 2021_06_15_063536) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2021_06_15_062955) do
     t.text "first_item"
     t.text "second_item"
     t.text "third_item"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
