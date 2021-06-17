eca7b2f1d60e143df9cefb4cc8505fce708e33ae
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-17 08:04:55 +0000

    [add]達成ステータス

--- app/controllers/admin/lists_controller.rb
+++ app/controllers/admin/lists_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ListsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_list, only:[:edit, :update, :destroy]
 
   def show
     @lists = List.all
@@ -7,15 +8,29 @@ class Admin::ListsController < ApplicationController
   end
 
   def edit
-    @list = List.find(params[:id])
     @user = User.find_by(id: @list.user_id)
+  end
+  
+  def update
+    @list.update(list_params)
+    flash[:primary] = "リストを更新しました"
+    redirect_to admin_list_path(@list)
   end
 
   def destroy
-    @list = List.find(params[:id])
     @user = User.find_by(id: @list.user_id)
     @list.destroy
     flash[:danger] = "投稿を削除しました"
     redirect_to admin_user_path(@user)
+  end
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:first_item, :second_item, :third_item, :date, :done)
+  end
+  
+  def set_list
+    @list = List.find(params[:id])
   end
 end
