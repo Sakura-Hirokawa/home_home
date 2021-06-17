b21d48658caf47226ed888cbf3f5fe672c0b8c06
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-17 05:30:35 +0000

    [fix]新規投稿リンク・いいねボタン

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '84298a1ed3f489c2117025007ff3b75464b012d15d88c93765f861dee3a58b10323e0df2f4a97c518da5e0c336542f8a2dd70519ca4ea6551ad87de5f2fa365e'
